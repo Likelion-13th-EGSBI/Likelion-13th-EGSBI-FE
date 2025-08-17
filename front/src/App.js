fe6c72d46ecb8259217901acbaa0685352d74030
yeoleum gang <gangyeoleum@gmail.com>
2025-08-17 22:44:54 +0900

    [feat] MainPage UI 작업

--- front/src/App.js
+++ front/src/App.js
@@ -18,8 +18,8 @@ import BookmarkedEvents from './pages/BookmarkedEvents';
 import EditProfile from './pages/EditProfile';
 import JoinedEvents from './pages/JoinedEvents';
 import EventEdit from './pages/EventEdit';
-import MainPage from './pages/InitMain';
 import Eventall from './pages/EventAll';
+import MainPage from './pages/MainPage';
 
 function App() {
   return (
@@ -43,8 +43,9 @@ function App() {
       <Route path="/my-upload-event" element={<MyUploadEvent/>} />
       <Route path="/event-edit/:eventId" element={<EventEdit />} />
       <Route path="/event-all" element={<Eventall />} />
+      <Route path="/main" element={<MainPage />} />
 
-
+s
 
 
 
